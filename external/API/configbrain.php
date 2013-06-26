f1c767bff9940c4cfdca9f48ab070fef3fe728e9
shngit <ypark@selfhealthnet.com>
2013-06-26 16:36:15 -0700

    brain game data extract update

--- external/API/configbrain.php
+++ external/API/configbrain.php
@@ -6,18 +6,28 @@ if( $testmode ){
 		define('DB_HOST', 'localhost');
 		define('DB_USERNAME', 'young');
 		define('DB_PASSWORD', 'Team2012mysql');		
-		define('DB_DATABASE', 'braingames_data');
+		define('DB_DATABASE', 'externaldata');
+		
+		define('DATA_DATABASE', 'braingames_data');
+		
 }else{
 		define('DB_HOST', '50.56.42.37');
 		define('DB_USERNAME', 'young');
 		define('DB_PASSWORD', 'Team2012mysql');		
 		define('DB_DATABASE', 'mpwg');
 		define('DB_DATABASE', 'braingames_data');
+		
+		define('DB_DATABASE', 'braingames_data');
+		
 }
 
 $DB_HOST = DB_HOST;
 $DB_USERNAME = DB_USERNAME;
 $DB_PASSWORD = DB_PASSWORD;
 $DB_NAME = DB_DATABASE;
+
+		define('DATA_DIR', '/storage/data/braingames/');
+		define('EXTRACT_DIR', DATA_DIR.'extracts/');
+		
 
 ?>
